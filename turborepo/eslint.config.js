e3b5be75cb7dcc04bbfa58f9d82c0e2b99f0c9ed
Dawid Perdek <perdekdawid@gmail.com>
2026-03-23 12:18:19 +0100

    fix lint in turborepo

--- turborepo/eslint.config.js
+++ turborepo/eslint.config.js
@@ -1,6 +1,22 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import angularPlugin from '@angular-eslint/eslint-plugin';
+import angularTemplatePlugin from '@angular-eslint/eslint-plugin-template';
+import templateParser from '@angular-eslint/template-parser';
+
 export default [
   {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      '@angular-eslint': angularPlugin,
+    },
     rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...angularPlugin.configs.recommended.rules,
       'no-restricted-imports': [
         'error',
         {
@@ -12,6 +28,18 @@ export default [
           ],
         },
       ],
+    },
+  },
+  {
+    files: ['**/*.html'],
+    languageOptions: {
+      parser: templateParser,
+    },
+    plugins: {
+      '@angular-eslint/template': angularTemplatePlugin,
+    },
+    rules: {
+      ...angularTemplatePlugin.configs.recommended.rules,
     },
   },
 ];
