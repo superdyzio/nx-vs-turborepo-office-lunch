7065f027fd4bcf8eb3d5546f591815887539c2b5
Dawid Perdek <perdekdawid@gmail.com>
2026-03-23 12:26:35 +0100

    fix build in turborepo

--- turborepo/apps/office-lunch/src/main.ts
+++ turborepo/apps/office-lunch/src/main.ts
@@ -3,7 +3,7 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { initDb } from '@office-lunch/util';
 
-(window as Window & { __initDb: typeof initDb }).__initDb = initDb;
+(window as unknown as { __initDb: typeof initDb }).__initDb = initDb;
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
